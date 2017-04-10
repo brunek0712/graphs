c1356be480c685a7a0b93981de051b44608be4b2
olsenp <olsenp@OLSENP.strose.edu>
2017-04-10 15:09:14 -0400

    added test graphs + a simple example.

---
+++ src/examples/PathVerification.java
@@ -0,0 +1,37 @@
+package examples;
+
+import java.util.Scanner;
+
+import graph.Edge;
+import graph.Graph;
+import graph.Vertex;
+/**
+ * This program prints nothing if the path is a valid one.  
+ * Otherwise it prints the first fictitious edge in the path.
+ * @author Paul Olsen
+ *
+ */
+public class PathVerification {
+	public static void main(String[] args) {
+		Graph g = new Graph(args[0]);
+		
+		Scanner scan = new Scanner(System.in);
+		
+		int next = scan.nextInt();
+		while(next != -1) {
+			Vertex v = g.get(next);
+			next = scan.nextInt();
+			if(next == -1) break;
+			boolean valid = false;
+			for(Edge e : v.outEdges()) {
+				if(e.destination().id() == next) {
+					valid = true;
+				}
+			}
+			if(!valid) {
+				System.out.println("No edge from Vertex #" + v.id() + " to Vertex#" + next);
+				break;
+			}
+		}
+	}
+}
