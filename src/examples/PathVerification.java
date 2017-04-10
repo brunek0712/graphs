fe55c68471aa7889d99e400e6781bb86859559a6
olsenp <olsenp@OLSENP.strose.edu>
2017-04-10 15:11:20 -0400

    bug fix.

--- src/examples/PathVerification.java
+++ src/examples/PathVerification.java
@@ -7,7 +7,7 @@ import graph.Graph;
 import graph.Vertex;
 /**
  * This program prints nothing if the path is a valid one.  
- * Otherwise it prints the first fictitious edge in the path.
+ * Otherwise it prints the first fictitious vertex or edge in the path.
  * @author Paul Olsen
  *
  */
@@ -20,6 +20,10 @@ public class PathVerification {
 		int next = scan.nextInt();
 		while(next != -1) {
 			Vertex v = g.get(next);
+			if(v == null) {
+				System.out.println("No Vertex #" + next);
+				break;
+			}
 			next = scan.nextInt();
 			if(next == -1) break;
 			boolean valid = false;
@@ -29,7 +33,7 @@ public class PathVerification {
 				}
 			}
 			if(!valid) {
-				System.out.println("No edge from Vertex #" + v.id() + " to Vertex#" + next);
+				System.out.println("No edge from Vertex #" + v.id() + " to Vertex #" + next);
 				break;
 			}
 		}
